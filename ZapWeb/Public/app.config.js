d4a84b319bad8a05cd897b2d25e9f20a614837ec
manoelneco <necoengenheiro@hotmail.com>
2015-06-20 15:53:41 -0300

    adicionada estrutura basica

---
+++ ZapWeb/Public/app.config.js
@@ -0,0 +1,40 @@
+﻿/**
+ * Configuration Globais
+ */
+Application.config({
+
+    ajax: {
+        contentType: 'application/json'
+    },
+
+    textbox: {
+        classes: 'form-control'
+    },
+
+    textarea: {
+        classes: 'form-control'
+    },
+
+    button: {
+        classes: 'btn btn-sm'
+    },
+
+    checkbox: {
+        classes: 'checkbox-inline i-checks'
+    }
+});
+
+PI.Url.add('BaseUrl', PI.Url.create(window.location.toString()).href());
+
+PI.Url.add('App', 'BaseUrl', '/Public');
+PI.Url.add('Modules', 'App', '/modules');
+PI.Url.add('Lib', 'App', '/lib');
+PI.Url.add('UI', 'Lib', '/pillar/ui');
+PI.Url.add('DataTime', 'Lib', '/datatime');
+PI.Url.add('Tips', 'Lib', '/tips');
+PI.Url.add('Scroll', 'Lib', '/scroll');
+
+/**
+ * Alias Url
+ */
+//PI.Url.add('Grupo', 'Modules', '/grupo');
---
+++ ZapWeb/Public/auth.js
@@ -0,0 +1,21 @@
+﻿yum.define([
+	PI.Url.create('App', '/ui.config.js')
+], function (html) {
+    
+    Class('App').Extend(Mvc.Component).Body({
+
+        didFinishLaunch: function () {
+           
+        },
+
+        progressLoad: function (total) {
+            
+        },
+
+        events: {
+
+        }
+
+    });
+
+});
